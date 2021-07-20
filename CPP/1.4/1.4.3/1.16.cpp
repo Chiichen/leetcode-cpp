a65e38c11f9dd320a8733894e61b5518dd9215a3
暗影精灵 <2531693734@qq.com>
2021-07-20 18:33:43 +0800

    1
    1

--- CPP/1.4/1.4.3/1.16.cpp
+++ CPP/1.4/1.4.3/1.16.cpp
@@ -2,7 +2,9 @@
 int main()
 {
     int sum=0,val=0;
-    while(s){
-
+    while(std::cin>>val){  //Windows系统中Ctrl+Z，unix和mac OS X中 Ctrl+D输入文件结束符
+    //或者输入小数来结束程序
+        sum += val;
     }
+    std::cout<<"Sum is "<<sum<<std::endl;
 }
