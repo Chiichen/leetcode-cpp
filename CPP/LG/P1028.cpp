f9db372326719896bfbf843c694dc47b326cb8d1
KevinIrene <2531693734@qq.com>
2021-11-10 22:19:17 +0800

    1

--- CPP/LG/P1028.cpp
+++ CPP/LG/P1028.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 #include<vector>
-#include<string>
-#include<stdlib.h>
+#include<string.h>
 using namespace std;
-int count(int num)
+int f[1003];
+int main()
+{
+    int n=0;
+    cin>>n;
+    memset(f,0,sizeof(f));
+    f[1]=1,f[2]=2;
+    for(int i=3;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            f[i]=f[i-1]+f[i/2];
+            
+        }
+        else if(i%2!=0)
+        {
+            f[i]=f[i-1];
+        }
+    }
+    cout<<f[n];
+
+    
+}
