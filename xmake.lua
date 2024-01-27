ca0b8825bbf8421672350965304e36aa4635518d
chiichen <chiichen@qq.com>
2024-01-27 20:56:19 +0800

    新增一个预编译宏来可提交的本地补全方案

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,7 @@ add_rules("mode.debug", "mode.release")
 target("src")
     set_kind("static")
     add_files("src/*.cpp")
+    add_defines("LEETCODELOCAL")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
