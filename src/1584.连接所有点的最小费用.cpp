e2e469e984fb00b6a1e88abd3e992a4bb1fca514
Chiichen <chiichen@qq.com>
2024-06-22 21:07:20 +0800

    feat: finish 1584 with Kruskal Algorithm

--- src/1584.连接所有点的最小费用.cpp
+++ src/1584.连接所有点的最小费用.cpp
@@ -5,41 +5,117 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <csetjmp>
+#include <iostream>
+#include <istream>
 #include <queue>
 #include <tuple>
+#include <utility>
 #include <vector>
 using namespace std;
 class Solution {
 public:
+  // /// Prim Algorithm
+  // int minCostConnectPoints(vector<vector<int>> &points) {
+  //   auto cmp = [](const tuple<int, int> &left, const tuple<int, int> &right)
+  //   {
+  //     return std::get<1>(left) > std::get<1>(right);
+  //   };
+  //   priority_queue<tuple<int, int>, vector<tuple<int, int>>, decltype(cmp)>
+  //   pq(
+  //       cmp);
+  //   vector<bool> visited = vector<bool>(points.size(), false);
+  //   auto cut = [&](int start) {
+  //     for (auto i = 0; i < points.size(); i++) {
+  //       if (visited[i] || start == i) {
+  //         continue;
+  //       }
+  //       pq.push({i, abs(points[start][0] - points[i][0]) +
+  //                       abs(points[start][1] - points[i][1])});
+  //     };
+  //   };
+  //   int cost = 0;
+  //   visited[0] = true;
+  //   cut(0);
+  //   while (!pq.empty()) {
+  //     auto point = pq.top();
+  //     pq.pop();
+  //     if (visited[std::get<0>(point)]) {
+  //       continue;
+  //     }
+  //     visited[std::get<0>(point)] = true;
+  //     cost += std::get<1>(point);
+  //     cut(std::get<0>(point));
+  //   };
+  //   return cost;
+  // }
+
+  /// Kruskal Algorithm
   int minCostConnectPoints(vector<vector<int>> &points) {
-    auto cmp = [](const tuple<int, int> &left, const tuple<int, int> &right) {
-      return std::get<1>(left) > std::get<1>(right);
-    };
-    priority_queue<tuple<int, int>, vector<tuple<int, int>>, decltype(cmp)> pq(
-        cmp);
-    vector<bool> visited = vector<bool>(points.size(), false);
-    auto cut = [&](int start) {
-      for (auto i = 0; i < points.size(); i++) {
-        if (visited[i] || start == i) {
-          continue;
+    class UF {
+    private:
+      vector<int> parent;
+
+    public:
+      UF(int size) {
+        parent.resize(size, 0);
+        for (auto i = 0; i < parent.size(); i++) {
+          parent[i] = i;
         }
-        pq.push({i, abs(points[start][0] - points[i][0]) +
-                        abs(points[start][1] - points[i][1])});
+      }
+      int find(int i) {
+        if (parent[i] != i) {
+          parent[i] = find(parent[i]);
+        }
+        return parent[i];
+      }
+      void join(int source, int target) {
+        auto ps = find(source);
+        auto pt = find(target);
+        if (ps == pt) {
+          return;
+        } else {
+          parent[ps] = pt;
+        }
+      }
+      bool is_connect(int source, int target) {
+        auto ps = find(source);
+        auto pt = find(target);
+        if (ps == pt) {
+          return true;
+        } else {
+          return false;
+        }
       };
     };
-    int cost = 0;
-    visited[0] = true;
-    cut(0);
-    while (!pq.empty()) {
-      auto point = pq.top();
-      pq.pop();
-      if (visited[std::get<0>(point)]) {
-        continue;
-      }
-      visited[std::get<0>(point)] = true;
-      cost += std::get<1>(point);
-      cut(std::get<0>(point));
+    auto cmp = [](const tuple<pair<int, int>, int> &left,
+                  const tuple<pair<int, int>, int> &right) {
+      return std::get<1>(left) < std::get<1>(right);
     };
+    vector<tuple<pair<int, int>, int>> a;
+    for (auto i = 0; i < points.size(); i++) {
+      for (auto j = 0; j < points.size(); j++) {
+        if (i == j) {
+          continue;
+        }
+        a.push_back({{i, j},
+                     abs(points[i][0] - points[j][0]) +
+                         abs(points[i][1] - points[j][1])});
+      }
+    }
+    sort(a.begin(), a.end(), cmp);
+    int cost = 0;
+    UF uf = UF(a.size());
+    for (auto i = 0; i < a.size(); i++) {
+      auto point = std::get<0>(a[i]);
+      if (uf.is_connect(point.first, point.second)) {
+        continue;
+      } else {
+        cost += std::get<1>(a[i]);
+        uf.join(point.first, point.second);
+      }
+    }
     return cost;
   }
 };
