9807b911c0b21649d3adffc24e71370d1813ca76
Chiichen <chiichen@qq.com>
2024-06-29 23:36:41 +0800

    feat: finish 518

---
+++ src/518.零钱兑换-ii.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+using namespace std;
+#include <vector>
+class Solution {
+public:
+  int change(int amount, vector<int> &coins) {
+    if (coins.size() == 0) {
+      return 0;
+    }
+
+    vector<vector<int>> dp =
+        vector<vector<int>>(coins.size() + 1, vector<int>(amount + 1, 0));
+    for (int i = 1; i <= coins.size(); i++)
+      dp[i][0] = 1;
+    for (auto i = 1; i <= coins.size(); i++) {
+      for (auto j = 1; j <= amount; j++) {
+        if (j - coins[i - 1] >= 0) {
+          dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+        } else {
+          dp[i][j] = dp[i - 1][j];
+        }
+      }
+    }
+    return dp[coins.size()][amount];
+  }
+};
+// @lc code=end
