1b83af9245391b137f4a169186141ff56c3578e5
Chiichen <chiichen@qq.com>
2024-06-30 11:49:40 +0800

    feat: better 518

--- src/518.零钱兑换-ii.cpp
+++ src/518.零钱兑换-ii.cpp
@@ -13,21 +13,30 @@ public:
     if (coins.size() == 0) {
       return 0;
     }
-
-    vector<vector<int>> dp =
-        vector<vector<int>>(coins.size() + 1, vector<int>(amount + 1, 0));
-    for (int i = 1; i <= coins.size(); i++)
-      dp[i][0] = 1;
+    vector<int> dp = vector<int>(amount + 1, 0);
+    dp[0] = 1;
     for (auto i = 1; i <= coins.size(); i++) {
       for (auto j = 1; j <= amount; j++) {
         if (j - coins[i - 1] >= 0) {
-          dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-        } else {
-          dp[i][j] = dp[i - 1][j];
+          dp[j] = dp[j] + dp[j - coins[i - 1]];
         }
       }
     }
-    return dp[coins.size()][amount];
+    return dp[amount];
+    // vector<vector<int>> dp =
+    //     vector<vector<int>>(coins.size() + 1, vector<int>(amount + 1, 0));
+    // for (int i = 1; i <= coins.size(); i++)
+    //   dp[i][0] = 1;
+    // for (auto i = 1; i <= coins.size(); i++) {
+    //   for (auto j = 1; j <= amount; j++) {
+    //     if (j - coins[i - 1] >= 0) {
+    //       dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+    //     } else {
+    //       dp[i][j] = dp[i - 1][j];
+    //     }
+    //   }
+    // }
+    // return dp[coins.size()][amount];
   }
 };
 // @lc code=end
